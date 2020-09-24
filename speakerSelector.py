c1868255bacd8d1b33f846114def4ad8322bd834
nevillejodie <jlneville92@gmail.com>
2020-09-24 12:44:35 +0100

    need to assign a random team member to each of the remaining fridays

--- speakerSelector.py
+++ speakerSelector.py
@@ -1,7 +1,3 @@
-""" 1. take in the names of staff members
-2. take in Friday dates 
-3. Arrange speaker name across the dates evenly
-4. returns the name of the speaker who's turn it is that week  """
 from datetime import date, timedelta
 import random
 
@@ -10,8 +6,8 @@ teamMember = random.choice(theTeam)
 print(teamMember)
 
 
- def allFridays(year):
-   d = date(year, 9, 21)                    # January 1st
+def allFridays(year):
+   d = date(year, 9, 24)                    # Todays date
    d += timedelta(days = 4 - d.weekday())  # First Friday
    while d.year == year:
       yield d
