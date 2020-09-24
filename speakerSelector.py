856839fef4cdb5a9a26f230aff1bb1ab7f3335cd
nevillejodie <jlneville92@gmail.com>
2020-09-24 13:59:29 +0100

    randomiser works

--- speakerSelector.py
+++ speakerSelector.py
@@ -2,18 +2,20 @@ from datetime import date, timedelta
 import random
 
 theTeam = ["Steve", "John", "Ken", "Jodie"]
-teamMember = random.choice(theTeam)
-print(teamMember)
+
+def getTeamMember():
+ teamMember = random.choice(theTeam)
+ print(teamMember)
 
 
 def allFridays(year):
-   d = date(year, 9, 24)                    # Todays date
+   d = date(year, 10, 2)                    # Todays date
    d += timedelta(days = 4 - d.weekday())  # First Friday
    while d.year == year:
       yield d
       d += timedelta(days = 7)
 
 for d in allFridays(2020):
- print (d) 
+ print (str(d), getTeamMember())
 
 
