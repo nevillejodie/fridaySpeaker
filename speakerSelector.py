213a9091804c256abca9a9ce872cde0b2601e40e
nevillejodie <jlneville92@gmail.com>
2020-09-24 12:05:08 +0100

    random team member

--- speakerSelector.py
+++ speakerSelector.py
@@ -3,14 +3,14 @@
 3. Arrange speaker name across the dates evenly
 4. returns the name of the speaker who's turn it is that week  """
 from datetime import date, timedelta
+import random
 
 theTeam = ["Steve", "John", "Ken", "Jodie"]
-#print(theTeam[0])
+teamMember = random.choice(theTeam)
+print(teamMember)
 
-for x in theTeam:
-    print(x)
 
-def allFridays(year):
+ def allFridays(year):
    d = date(year, 9, 21)                    # January 1st
    d += timedelta(days = 4 - d.weekday())  # First Friday
    while d.year == year:
@@ -18,6 +18,6 @@ def allFridays(year):
       d += timedelta(days = 7)
 
 for d in allFridays(2020):
-   print(d)
+ print (d) 
 
 
